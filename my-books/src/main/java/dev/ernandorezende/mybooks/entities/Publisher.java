524577905561c2484bff24fcad4bf49ce6378323
Ernando Rezende <ernando.03@gmail.com>
2024-12-22 22:35:51 +0000

    Adjusts on Publisherr manager feature

--- my-books/src/main/java/dev/ernandorezende/mybooks/entities/Publisher.java
+++ my-books/src/main/java/dev/ernandorezende/mybooks/entities/Publisher.java
@@ -1,8 +1,6 @@
 package dev.ernandorezende.mybooks.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.util.Objects;
 
@@ -11,6 +9,7 @@ import java.util.Objects;
 public class Publisher {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
 
