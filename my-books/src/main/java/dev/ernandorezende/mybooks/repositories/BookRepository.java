ac2e39b1092f299c2f5087f6a4ee4dcc823237cc
Ernando Rezende <ernando.03@gmail.com>
2024-12-27 00:15:47 +0000

    Created BookRepository

---
+++ my-books/src/main/java/dev/ernandorezende/mybooks/repositories/BookRepository.java
@@ -0,0 +1,7 @@
+package dev.ernandorezende.mybooks.repositories;
+
+import dev.ernandorezende.mybooks.entities.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
