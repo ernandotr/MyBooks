be0a0f0db4278af2116191c9ef5146e69c88f0c0
Ernando Rezende <ernando.03@gmail.com>
2025-01-01 01:28:12 +0000

    Created unit test for Book update method and impoved bussiness logic

--- my-books/src/main/java/dev/ernandorezende/mybooks/services/BookService.java
+++ my-books/src/main/java/dev/ernandorezende/mybooks/services/BookService.java
@@ -5,6 +5,9 @@ import dev.ernandorezende.mybooks.dtos.responses.BookResponse;
 import dev.ernandorezende.mybooks.entities.Author;
 import dev.ernandorezende.mybooks.entities.Book;
 import dev.ernandorezende.mybooks.entities.Publisher;
+import dev.ernandorezende.mybooks.exceptions.AuthorNotFoundException;
+import dev.ernandorezende.mybooks.exceptions.BookNotFoundException;
+import dev.ernandorezende.mybooks.exceptions.PublisherNotFoundException;
 import dev.ernandorezende.mybooks.repositories.AuthorRepository;
 import dev.ernandorezende.mybooks.repositories.BookRepository;
 import dev.ernandorezende.mybooks.repositories.PublisherRepository;
@@ -24,7 +27,6 @@ public class BookService {
 
     public BookService(BookRepository bookRepository, ModelMapper modelMapper,
                        AuthorRepository authorRepository, PublisherRepository publisherRepository) {
-
         this.bookRepository = bookRepository;
         this.modelMapper = modelMapper;
         this.authorRepository = authorRepository;
@@ -34,12 +36,32 @@ public class BookService {
     public BookResponse save(BooksRequest booksRequest) {
         Author author = authorRepository.getReferenceById(booksRequest.getAuthor());
         Publisher publisher = publisherRepository.getReferenceById(booksRequest.getPublisher());
+
         Book book = toEntity(booksRequest);
         book.setAuthor(author);
         book.setPublisher(publisher);
         book = bookRepository.save(book);
 
         return toResponse(book);
+    }
+
+    public void update(BooksRequest booksRequest, Long id) {
+            Author author = authorRepository.findById(booksRequest.getAuthor()).orElseThrow(AuthorNotFoundException::new);
+            Publisher publisher = publisherRepository.findById(booksRequest.getPublisher()).orElseThrow(PublisherNotFoundException::new);
+            Book book = bookRepository.findById(id).orElseThrow(BookNotFoundException::new);
+        try {
+            book.setAuthor(author);
+            book.setPublisher(publisher);
+            book.setTitle(booksRequest.getTitle());
+            book.setIsbn(booksRequest.getIsbn());
+            book.setGenre(booksRequest.getGenre());
+            book.setLanguage(booksRequest.getLanguage());
+            book.setPages(booksRequest.getPages());
+            book.setUrl(booksRequest.getUrl());
+            bookRepository.save(book);
+        } catch (Exception e) {
+            throw new RuntimeException("Could not update book", e);
+        }
     }
 
     public List<BookResponse> getAll() {
--- my-books/src/test/java/dev/ernandorezende/mybooks/service/BookServiceTest.java
+++ my-books/src/test/java/dev/ernandorezende/mybooks/service/BookServiceTest.java
@@ -2,8 +2,12 @@ package dev.ernandorezende.mybooks.service;
 
 import dev.ernandorezende.mybooks.dtos.requests.BooksRequest;
 import dev.ernandorezende.mybooks.dtos.responses.BookResponse;
+import dev.ernandorezende.mybooks.entities.Author;
 import dev.ernandorezende.mybooks.entities.Book;
+import dev.ernandorezende.mybooks.entities.Publisher;
+import dev.ernandorezende.mybooks.repositories.AuthorRepository;
 import dev.ernandorezende.mybooks.repositories.BookRepository;
+import dev.ernandorezende.mybooks.repositories.PublisherRepository;
 import dev.ernandorezende.mybooks.services.BookService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -15,9 +19,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 //@SpringBootTest
 @ExtendWith(MockitoExtension.class)
@@ -28,12 +33,16 @@ public class BookServiceTest {
 
     @Mock
     BookRepository bookRepository;
+    @Mock
+    AuthorRepository authorRepository;
+    @Mock
+    PublisherRepository publisherRepository;
 
     @InjectMocks
     BookService bookService;
 
     @Test
-    void saveBook() {
+    void saveBookSuccess() {
         BooksRequest booksRequest = new BooksRequest();
         booksRequest.setTitle("Title");
 
@@ -53,6 +62,25 @@ public class BookServiceTest {
         Assertions.assertNotNull(response);
         Assertions.assertEquals(1, response.size());
         Assertions.assertEquals(book.getTitle(), response.get(0).getTitle());
+    }
+
+    @Test
+    void updateBookSuccess() {
+        BooksRequest booksRequest = new BooksRequest();
+        booksRequest.setTitle("Title");
+
+        Book book = buildExpectedBook(1L, "Title1");
+        Author author = new Author("Author1");
+        Publisher publisher = new Publisher("Publisher1");
+
+        when(authorRepository.findById(anyLong())).thenReturn(Optional.of(author));
+        when(publisherRepository.findById(anyLong())).thenReturn(Optional.of(publisher));
+        when(bookRepository.findById(anyLong())).thenReturn(Optional.of(book));
+        when(bookRepository.save(any(Book.class))).thenReturn(book);
+
+        bookService.update(booksRequest, 1L);
+
+        verify(bookRepository, times(1)).save(any(Book.class));
     }
 
     private static Book buildExpectedBook(Long id, String title) {
