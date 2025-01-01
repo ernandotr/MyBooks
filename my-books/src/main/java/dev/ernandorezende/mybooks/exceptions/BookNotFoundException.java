e03c7bd36bcb5d6929c3e83d9af529287073c4c0
Ernando Rezende <ernando.03@gmail.com>
2025-01-01 01:10:28 +0000

    Created Customer exception for book not found

---
+++ my-books/src/main/java/dev/ernandorezende/mybooks/exceptions/BookNotFoundException.java
@@ -0,0 +1,10 @@
+package dev.ernandorezende.mybooks.exceptions;
+
+public class BookNotFoundException extends RuntimeException {
+    public BookNotFoundException(String message) {
+        super(message);
+    }
+    public BookNotFoundException() {
+        super("Book not found");
+    }
+}
