a04f6528c141bfdbeed7b2fe26a5566e45f0b9c5
Ernando Rezende <ernando.03@gmail.com>
2025-01-06 21:32:38 +0000

    Removed unnecessary code

--- my-books/src/main/java/dev/ernandorezende/mybooks/controllers/AuthorController.java
+++ my-books/src/main/java/dev/ernandorezende/mybooks/controllers/AuthorController.java
@@ -15,7 +15,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -40,7 +39,7 @@ public class AuthorController {
                     content = @Content(schema = @Schema(implementation = ErrorDetails.class)))
     })
     @GetMapping(produces = {"application/json", "application/xml"})
-    public ResponseEntity<Page<Author>> getAllAuthors(@PageableDefault(page = 0, size = 10) Pageable pageable ) {
+    public ResponseEntity<Page<Author>> getAllAuthors(Pageable pageable ) {
         return ResponseEntity.ok(authorService.getAll(pageable));
     }
 
