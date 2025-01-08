25fdc595079761eeda34081d73b400207f203989
Ernando Rezende <ernando.03@gmail.com>
2025-01-08 22:15:19 +0000

    Added swagger documentation for create book endpoint

--- my-books/src/main/java/dev/ernandorezende/mybooks/controllers/BookController.java
+++ my-books/src/main/java/dev/ernandorezende/mybooks/controllers/BookController.java
@@ -4,14 +4,22 @@ import dev.ernandorezende.mybooks.dtos.requests.BooksRequest;
 import dev.ernandorezende.mybooks.dtos.responses.BookResponse;
 import dev.ernandorezende.mybooks.dtos.responses.BookSummaryResponse;
 import dev.ernandorezende.mybooks.services.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Book", description = "The book operations.")
 @RestController
 @RequestMapping("api/books")
 public class BookController {
@@ -21,10 +29,17 @@ public class BookController {
         this.bookService = bookService;
     }
 
+    @Operation(summary = "Save a new book", description = "Allows the use to record a new book information.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successful operation."),
+            @ApiResponse(responseCode = "500", description = "Internal errors.",
+                    content = { @Content(schema = @Schema(implementation = ErrorResponse.class))})
+    })
     @PostMapping
     public ResponseEntity<BookResponse> createBook(@RequestBody BooksRequest booksRequest) {
         return ResponseEntity.status(HttpStatus.CREATED).body(bookService.create(booksRequest));
     }
+
 
     @GetMapping
     public ResponseEntity<Page<BookSummaryResponse>> getAllBooks(Pageable pageable) {
