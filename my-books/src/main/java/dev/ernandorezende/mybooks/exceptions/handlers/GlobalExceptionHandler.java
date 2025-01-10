b5cf674420ba0eeb51cdfe297ef649f2724006cc
Ernando Rezende <ernando.03@gmail.com>
2025-01-10 21:41:17 +0000

    Added a new ExceptionHandler method for validatios errors

--- my-books/src/main/java/dev/ernandorezende/mybooks/exceptions/handlers/GlobalExceptionHandler.java
+++ my-books/src/main/java/dev/ernandorezende/mybooks/exceptions/handlers/GlobalExceptionHandler.java
@@ -3,13 +3,16 @@ package dev.ernandorezende.mybooks.exceptions.handlers;
 import dev.ernandorezende.mybooks.exceptions.AuthorNotFoundException;
 import dev.ernandorezende.mybooks.exceptions.BookNotFoundException;
 import dev.ernandorezende.mybooks.exceptions.PublisherNotFoundException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -21,6 +24,18 @@ public class GlobalExceptionHandler {
                 ex.getMessage(),
                 request.getDescription(false));
         return ResponseEntity.internalServerError().body(errorDetails);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDetails> handleGlobalException(MethodArgumentNotValidException ex, WebRequest request) {
+        var errorDetails = new ErrorDetails(
+                LocalDateTime.now(),
+                ex.getBindingResult().getAllErrors().stream()
+                        .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                        .collect(Collectors.joining("\n")),
+                request.getDescription(false)
+        );
+        return ResponseEntity.badRequest().body(errorDetails);
     }
 
     @ExceptionHandler(AuthorNotFoundException.class)
