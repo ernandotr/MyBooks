4ce12921ef95ed7dc8c3d48167feb580628253d1
Ernando Rezende <ernando.03@gmail.com>
2025-01-16 22:21:19 +0000

    Improvement on validation fields error response

--- my-books/src/main/java/dev/ernandorezende/mybooks/exceptions/handlers/GlobalExceptionHandler.java
+++ my-books/src/main/java/dev/ernandorezende/mybooks/exceptions/handlers/GlobalExceptionHandler.java
@@ -3,39 +3,41 @@ package dev.ernandorezende.mybooks.exceptions.handlers;
 import dev.ernandorezende.mybooks.exceptions.AuthorNotFoundException;
 import dev.ernandorezende.mybooks.exceptions.BookNotFoundException;
 import dev.ernandorezende.mybooks.exceptions.PublisherNotFoundException;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
-import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorDetails> handleGlobalException(Exception ex, WebRequest request) {
-        var errorDetails = new ErrorDetails(
+    public ErrorDetails handleGlobalException(Exception ex, WebRequest request) {
+        return new ErrorDetails(
                 LocalDateTime.now(),
                 ex.getMessage(),
                 request.getDescription(false));
-        return ResponseEntity.internalServerError().body(errorDetails);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorDetails> handleGlobalException(MethodArgumentNotValidException ex, WebRequest request) {
-        var errorDetails = new ErrorDetails(
-                LocalDateTime.now(),
-                ex.getBindingResult().getAllErrors().stream()
-                        .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                        .collect(Collectors.joining("\n")),
-                request.getDescription(false)
-        );
-        return ResponseEntity.badRequest().body(errorDetails);
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(AuthorNotFoundException.class)
